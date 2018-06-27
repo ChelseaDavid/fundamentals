8b100e93fdb35786074d07fee6da3d7425fad7ae
Chelsea David <cryan17@cnm.edu>
2018-06-27 13:19:59 -0600

    objects and api review

---
+++ objects-and-apis.js
@@ -0,0 +1,26 @@
+/*
+ *Objects, Interfaces, and API's
+ */
+/*
+let dog = {
+	name: "Opie",
+	color: "Cow",
+	breed: "Shitzu-Chihuahua",
+	size: "Small",
+	bark: function (typeOfBark){
+		console.log("Bark!");
+	},
+};
+*/
+/*
+function x(y) {
+	y.num = y.num + 5;
+	console.log(y);
+}
+let y = {
+	name: 'Chelsea',
+	num: 10,
+}
+x(y);
+console.log(y);
+*/
---
+++ variables-and-data.js
@@ -0,0 +1,43 @@
+/*
+ *Variables, Data Types, and Typing
+
+let wordData = "Word list";
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
+
+console.log(totalWithTax);
+*/
+/*
+let obj = {
+	key1: "Value 1",
+	key2: 4,
+	boolean: true,
+	obj2: {
+		obj2Key1: "Value of Inner Object",
+	},
+};
+
+console.log(obj.obj2.obj2Key1);
+*/
+/*
+let var1 = "2";
+let var2 = 5;
+let result = var1 + var2;
+
+console.log(result);
+*/
+/*
+let add = 50 + 50;
+let sub = 2 - 1;
+let mult = 2*4;
+let division = 9 / 3;
+let mod = 5 % 2;
+
+let string = "Chelsea is the smartest woman alive. " + "Matthew strongly aggrees " + add + "%";
+
+console.log(string);
+*/
